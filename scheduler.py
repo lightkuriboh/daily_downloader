d2e5029c0c2f4a9f02a9da70fcd3da84c7304755
kuribohkute <kuribohwithwing@gmail.com>
2021-01-30 13:52:13 +0700

    Complete

--- scheduler.py
+++ scheduler.py
@@ -8,6 +8,8 @@ import utils
 class JobScheduler:
     def __init__(self, specific_time, job, first_time_run_params, future_run_params):
         self.seconds = utils.datetime_to_second(specific_time)
+        if self.seconds == 0:
+            self.seconds = 60 * 60 * 24
         self.job = job
         self.job(**first_time_run_params)
         self.future_run_params = future_run_params
@@ -17,13 +19,14 @@ class JobScheduler:
 
     def __run(self):
         current_seconds = utils.datetime_to_second(datetime.datetime.now())
-        if current_seconds < self.seconds:
+        if self.seconds > current_seconds:
             if self.seconds - current_seconds > 2:
                 time.sleep(self.seconds - current_seconds - 2)
             else:
                 self.job(**self.future_run_params)
         else:
             day_end = datetime.datetime.now().replace(hour=23, minute=23, second=59)
+            # sleep to end of day
             time.sleep(
                 utils.datetime_to_second(day_end) - current_seconds
             )
